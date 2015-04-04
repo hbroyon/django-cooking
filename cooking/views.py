c202df150931856a93d8ae45251a2d1c46441a11
William McKIE <mckie.william@hotmail.co.uk>
2015-04-04 12:40:11 +0200

    Added the index view.
    Added the IndexView class into the urlpatterns variable as root url.

--- cooking/views.py
+++ cooking/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.views import generic
+
+from cooking.models import Recipe
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    """
+    The main view.
+    """
+    model = Recipe
+    template_name = "cooking/index.html"
+    pk_url_kwarg = 'id'
