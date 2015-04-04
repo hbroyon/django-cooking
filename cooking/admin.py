37fcf0421749c5aa4df835b27d6f63506637b2ba
William McKIE <mckie.william@hotmail.co.uk>
2015-04-04 12:26:20 +0200

    Registered the models for the admin usage.

--- cooking/admin.py
+++ cooking/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from cooking.models import Ingredient, RecipeDifficulty, RecipeType, \
+    RecipePhoto, Recipe, RecipeComment
+
+
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(RecipeDifficulty)
+admin.site.register(RecipeType)
+admin.site.register(RecipePhoto)
+admin.site.register(Recipe)
+admin.site.register(RecipeComment)
